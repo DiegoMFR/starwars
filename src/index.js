31a4f0036d9cdd3b77f5c0e51e85dc9449840eb0
Diego Morales <diegomorales@MacBook-Pro-de-Stefania.local>
2020-06-11 07:59:06 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import './style.scss';
+
+function component() {
+    fetch('http://swapi.dev/api/people/?page=1')
+        .then(response => response.json())
+        .then(data => populate(data));
+}
+
+function populate(data) {
+    const container = document.createElement('section');
+    const item = document.createElement('div');
+    let html = '';
+    for(const dataEl of data.results) {
+        html += createItem(dataEl);
+    }
+   
+    container.innerHTML = html;
+    container.classList.add('main-container');
+    document.body.appendChild(container);
+}
+
+function createItem(dataEl) {
+    
+        const {name, height, homeworld} = dataEl;
+        return (
+            `<div class = "main-container--item">
+                <div class="main-container--item--content">
+                    <p> Name: ${name} </p>
+                    <p> height: ${height} </p>
+                    <p> homeworld: ${homeworld} </p>
+                </div>
+            </div>`
+        );
+}
+component();
